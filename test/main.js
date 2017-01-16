d4e162beccc420aee86e84ad2ea1950b77c84dc2
Alexander Treptau <atreptau@welfenlab.de>
2017-01-16 18:28:52 +0100

    Initial code commit

---
+++ test/main.js
@@ -0,0 +1,11 @@
+
+/* global describe, it */
+// import * as Graph from '@buggyorg/graphtools'
+// import chai from 'chai'
+// import chaiAsPromised from 'chai-as-promised'
+
+describe('TODO', () => {
+  describe('TODO', () => {
+    it('TODO', () => { })
+  })
+})
